b3250f3ac96b7f8d4fdd530400b29ddd2a7e3bdc
Trionix BD <116764522+Trionix-BD@users.noreply.github.com>
2022-12-05 17:43:19 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+var A = document.getElementById('A');
+A.addEventListener("click",function(){
+    document.getElementById('AA').play();
+});
+
+var B = document.getElementById('B');
+B.addEventListener("click",function(){
+    document.getElementById('BB').play();
+});
+
+var C = document.getElementById('C');
+C.addEventListener("click",function(a){
+    document.getElementById('CC').play();
+});
+
+var D = document.getElementById('D');
+D.addEventListener("click",function(){
+    document.getElementById('DD').play();
+});
+
+var E = document.getElementById('E');
+E.addEventListener("click",function(){
+    document.getElementById('EE').play();
+});
+
+var F = document.getElementById('F');
+F.addEventListener("click",function(){
+    document.getElementById('FF').play();
+});
+
+var G = document.getElementById('G');
+G.addEventListener("click",function(){
+    document.getElementById('GG').play();
+});
+
+var H = document.getElementById('H');
+H.addEventListener("click",function(){
+    document.getElementById('HH').play();
+});
+
+var I = document.getElementById('I');
+I.addEventListener("click",function(){
+    document.getElementById('II').play();
+});
+
+var J = document.getElementById('J');
+J.addEventListener("click",function(){
+    document.getElementById('JJ').play();
+});
+
+var K = document.getElementById('K');
+K.addEventListener("click",function(){
+    document.getElementById('KK').play();
+});
+
+var L = document.getElementById('L');
+L.addEventListener("click",function(){
+    document.getElementById('LL').play();
+});
+
+var M = document.getElementById('M');
+M.addEventListener("click",function(){
+    document.getElementById('MM').play();
+});
+
+var N = document.getElementById('N');
+N.addEventListener("click",function(){
+    document.getElementById('NN').play();
+});
+
+var O = document.getElementById('O');
+O.addEventListener("click",function(){
+    document.getElementById('OO').play();
+});
+
+var P = document.getElementById('P');
+P.addEventListener("click",function(){
+    document.getElementById('PP').play();
+});
+
+var Q = document.getElementById('Q');
+Q.addEventListener("click",function(){
+    document.getElementById('QQ').play();
+});
+
+var R = document.getElementById('R');
+R.addEventListener("click",function(){
+    document.getElementById('RR').play();
+});
+
+var S = document.getElementById('S');
+S.addEventListener("click",function(){
+    document.getElementById('SS').play();
+});
+
+var T = document.getElementById('T');
+T.addEventListener("click",function(){
+    document.getElementById('TT').play();
+});
+
+var U = document.getElementById('U');
+U.addEventListener("click",function(){
+    document.getElementById('UU').play();
+});
+
+var V = document.getElementById('V');
+V.addEventListener("click",function(){
+    document.getElementById('VV').play();
+});
+
+var W = document.getElementById('W');
+W.addEventListener("click",function(){
+    document.getElementById('WW').play();
+});
+
+var X = document.getElementById('X');
+X.addEventListener("click",function(){
+    document.getElementById('XX').play();
+});
+
+var Y = document.getElementById('Y');
+Y.addEventListener("click",function(){
+    document.getElementById('YY').play();
+});
+
+var Z = document.getElementById('Z');
+Z.addEventListener("click",function(){
+    document.getElementById('ZZ').play();
+});
